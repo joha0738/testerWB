07d030ea3d1c82221284acb196b40c1f1c92f96d
joha <churbanow.lesha@yandex.ru>
2021-10-23 20:00:02 +0300

    Изменил DB  на Н2 добавил отдельную регистрацию для админа и юзера

--- src/main/java/com/example/tester/config/WebSecurityConfig.java
+++ src/main/java/com/example/tester/config/WebSecurityConfig.java
@@ -31,6 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
 
--- src/main/java/com/example/tester/controllers/SignController.java
+++ src/main/java/com/example/tester/controllers/SignController.java
@@ -6,9 +6,7 @@ import com.example.tester.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
@@ -24,7 +22,8 @@ public class SignController {
         return "registration";
     }
 
-    @PostMapping("/registration")
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST, params = "regUser")
     public String postRegistration(User user, Model model,@RequestParam String username) {
 
         if(userRepository.findByUsername(username) != null){
@@ -35,6 +34,20 @@ public class SignController {
             user.setRoles(Collections.singleton(Role.ROLE_USER));
             user.setActive(true);
             userRepository.save(user);
+        return "redirect:/login";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST, params = "regAdmin")
+    public String postRegistrationAdmin(User user, Model model,@RequestParam String username) {
+
+        if(userRepository.findByUsername(username) != null){
+            model.addAttribute("message","Пользователь с данным логином уже существует!");
+            return "registration";
+        }
+
+        user.setRoles(Collections.singleton(Role.ROLE_ADMIN));
+        user.setActive(true);
+        userRepository.save(user);
         return "redirect:/login";
     }
 
